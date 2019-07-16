b66f53a5e5d84adcaa20d4e1ce81af7ccdd4aa88
Rodrigo De Rosa <rderosa@fi.uba.ar>
2019-07-16 18:03:29 -0300

    commented followers by candidate queries

--- Mini-BackEnd/src/api/DashboardResource.py
+++ Mini-BackEnd/src/api/DashboardResource.py
@@ -20,14 +20,14 @@ class DashboardResource(Resource):
         # Get count of active users
         active_users = RawFollowerDAO().get_count({'has_tweets': True})
         # Get count of followers for each candidate
-        candidates = list(map(lambda c: c.screen_name, CandidateService().get_all()))
-        followers_by_candidate = dict()
-        for candidate in candidates:
-            followers = RawFollowerDAO().get_count({'follows': candidate})
-            active_followers = RawFollowerDAO().get_count({'follows': candidate, 'has_tweets': True})
-            followers_by_candidate[candidate] = {'followers': followers,
-                                                 'active_followers': active_followers,
-                                                 'proportion': active_followers / followers}
+        # candidates = list(map(lambda c: c.screen_name, CandidateService().get_all()))
+        # followers_by_candidate = dict()
+        # for candidate in candidates:
+        #     followers = RawFollowerDAO().get_count({'follows': candidate})
+        #     active_followers = RawFollowerDAO().get_count({'follows': candidate, 'has_tweets': True})
+        #     followers_by_candidate[candidate] = {'followers': followers,
+        #                                          'active_followers': active_followers,
+        #                                          'proportion': active_followers / followers}
         # Get count of found topics
         topics = CooccurrenceGraphDAO().get_count({'topic_id': {'$ne': 'main'}})
         # Get count of known hashtags
@@ -39,7 +39,7 @@ class DashboardResource(Resource):
                     'total_users': users,
                     'active_users': active_users,
                     'active_proportion': active_users / users,
-                    'followers_by_candidate': followers_by_candidate,
+                    'followers_by_candidate': {},  # followers_by_candidate,
                     'topic_count': topics,
                     'hashtag_count': hashtags,
                     'cooccurrences_count': cooccurrences}
