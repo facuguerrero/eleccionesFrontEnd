87b2aec490de44c93628c7fe198208af1f950244
Rodrigo De Rosa <rderosa@fi.uba.ar>
2019-09-04 17:05:45 -0300

    separate dashboard response

--- Mini-BackEnd/src/api/DashboardResource.py
+++ Mini-BackEnd/src/api/DashboardResource.py
@@ -1,3 +1,4 @@
+from flask import request
 from flask_restful import Resource
 
 from src.db.daos.CooccurrenceDAO import CooccurrenceDAO
@@ -11,22 +12,35 @@ class DashboardResource(Resource):
 
     @staticmethod
     def get():
+        data = request.args.get('data')
+        if data == 'numbers':
+            response = DashboardResource.numbers()
+        elif data == 'graphics':
+            response = DashboardResource.graphics()
+        else:
+            response = {**DashboardResource.numbers(), **DashboardResource.graphics()}
+        # Respond request
+        return ResponseBuilder.build(response, 200)
+
+    @staticmethod
+    def numbers():
         # Get count of analyzed tweets
         tweets = RawTweetDAO().get_count({}) + 481267890  # Count of deleted tweets
         # Get count of known hashtags
         hashtags = HashtagDAO().get_count({})
         # Get count of hashtag cooccurrences
         cooccurrences = CooccurrenceDAO().get_count({})
+        return {'tweets': tweets,
+                'hashtag_count': hashtags,
+                'cooccurrences_count': cooccurrences}
+
+    @staticmethod
+    def graphics():
         # Retrieve dashboard data from database
         dashboard_data = DashboardDAO().dashboard_data()
         # Build response object
-        response = {'tweets': tweets,
-                    'total_users': dashboard_data['users'],
-                    'active_users': dashboard_data['active_users'],
-                    'active_proportion': dashboard_data['active_proportion'],
-                    'followers_by_candidate': dashboard_data['followers_by_candidate'],
-                    'topic_count': dashboard_data['topics'],
-                    'hashtag_count': hashtags,
-                    'cooccurrences_count': cooccurrences}
-        # Respond request
-        return ResponseBuilder.build(response, 200)
+        return {'total_users': dashboard_data['users'],
+                'active_users': dashboard_data['active_users'],
+                'active_proportion': dashboard_data['active_proportion'],
+                'followers_by_candidate': dashboard_data['followers_by_candidate'],
+                'topic_count': dashboard_data['topics']}
