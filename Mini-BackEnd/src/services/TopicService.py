084ae6ce3c983ace24ab3c8c7af294f8642564cd
Rodrigo De Rosa <rderosa@fi.uba.ar>
2019-09-20 17:11:37 -0300

    normalized node size

--- Mini-BackEnd/src/services/TopicService.py
+++ Mini-BackEnd/src/services/TopicService.py
@@ -4,10 +4,27 @@ from src.util.DateUtils import DateUtils
 
 class TopicService:
 
+    MAX_SIZE = 2500
+    MIN_SIZE = 300
+
     @classmethod
     def find_topic(cls, topic_id, start_date, end_date=None):
         """ Find showable graph for time window and topic_id. """
         # Parse end date to match database values
         end_date = DateUtils.last_second_of_day(end_date if end_date else start_date)
         # Retrieve topic graph
-        return ShowableGraphDAO().find(topic_id, start_date, end_date)
+        graph = ShowableGraphDAO().find(topic_id, start_date, end_date)
+        # Normalize node size
+        nodes = graph['nodes']
+        sizes = map(lambda node: node['size'], nodes)
+        max_size = max(sizes)
+        # Normalize to a (0,1] vector
+        for node in nodes:
+            node['size'] = (node['size'] / max_size)
+        # Subtract minimum value to get effective [0,1) vector and transform to wanted interval
+        sizes = map(lambda node: node['size'], nodes)
+        min_size = min(sizes)
+        max_size = max(sizes)
+        for node in nodes:
+            node['size'] = ((node['size'] - min_size)/max_size)*cls.MAX_SIZE + cls.MIN_SIZE
+        return graph
